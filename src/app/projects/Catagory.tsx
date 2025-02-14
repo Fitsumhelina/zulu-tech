e649c7a4d7c729a206172a983184c5fabc2eb3d1
Fitsumhelina <permin1501424@gmail.com>
2025-02-14 22:29:56 +0300

    feat: ui update

--- src/app/projects/Catagory.tsx
+++ src/app/projects/Catagory.tsx
@@ -37,7 +37,7 @@ function Category() {
   return (
   <>
     {/* Navigation */}
-    <div className="relative max-w-7xl mx-auto px-4 py-4  bg-white-500 overflow-hidden mt-10">
+    <div className="relative max-w-8xl mx-auto px-4 py-4  bg-white-500 overflow-hidden mt-10 mr-6 ml-6">
     <div className="flex items-center relative">
       <Button
       size="icon"
